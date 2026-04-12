5c097ea8026c533d523666ac721936a475a8b051
AnaZec <ana.zecns@gmail.com>
2026-04-13 01:42:11 +0200

    #22 refactor(dct): update dct.hpp for entropy encoding integration

--- include/dct.hpp
+++ include/dct.hpp
@@ -1,31 +1,17 @@
 #pragma once
 
+#include "block_splitter.hpp"
+
 #include <array>
 #include <vector>
-#include <cstdint>
 
-// 8x8 spatial block (input)
-using Block8x8 = std::array<uint8_t, 64>;
-
-// 8x8 frequency block (output)
+// 8x8 frequency block (output of DCT)
 using DctBlock8x8 = std::array<double, 64>;
-
-struct ChannelBlocks {
-    int blocksPerRow = 0;
-    int blocksPerCol = 0;
-    std::vector<Block8x8> blocks;
-};
 
 struct DctChannelBlocks {
     int blocksPerRow = 0;
     int blocksPerCol = 0;
     std::vector<DctBlock8x8> blocks;
-};
-
-struct ImageBlocks {
-    ChannelBlocks yBlocks;
-    ChannelBlocks cbBlocks;
-    ChannelBlocks crBlocks;
 };
 
 struct DctImageBlocks {
